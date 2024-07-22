a7541af741ea3d95aecf564e54ae4b10fe40357a
Sanjeev Gohil <118007831+sanjeevgohil@users.noreply.github.com>
2024-07-22 13:47:04 +0530

    Add files via upload

---
+++ connected/ConnectedDisplay.aspx.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Web.Configuration;
+
+public partial class ConnectedDisplay : System.Web.UI.Page
+{
+    string x = WebConfigurationManager.ConnectionStrings["cn"].ConnectionString.ToString();
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if(!IsPostBack)
+        {
+            ddid.Items.Clear();
+
+            SqlConnection con = new SqlConnection(x);
+
+            con.Open();
+            string sql = "select * from stud";
+
+            SqlCommand cmd = new SqlCommand(sql,con);
+            SqlDataReader dr;
+
+            dr = cmd.ExecuteReader();
+
+            if (dr.HasRows)
+            {
+                while (dr.Read())
+                {
+                    ddid.Items.Add(dr["id"].ToString());
+                }
+                ddid.SelectedIndex = 0;
+            }
+            else
+            {
+                Response.Write("No Record Found....");
+            }
+            dr.Close();
+            con.Close();
+        }
+    }
+    protected void ddid_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        SqlConnection con = new SqlConnection(x);
+        con.Open();
+
+        string sql = "select * from stud where id=@id";
+
+        SqlCommand cmd = new SqlCommand(sql,con);
+
+        cmd.Parameters.AddWithValue("@id",ddid.SelectedValue);
+
+        SqlDataReader dr;
+
+        dr = cmd.ExecuteReader();
+
+        if(dr.HasRows)
+        {
+            dr.Read();
+            txtid.Text = dr["id"].ToString();
+            txtnm.Text = dr["name"].ToString();
+            txtcity.Text = dr["city"].ToString();
+        }
+        dr.Close();
+        con.Close();
+    }
+}
---
+++ connected/DisconnectedInsert.aspx.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using System.Data.SqlClient;
+using System.Web.Configuration;
+using System.Data;
+
+public partial class Default2 : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            string x = WebConfigurationManager.ConnectionStrings["cn"].ConnectionString;
+
+            SqlConnection cn = new SqlConnection(x);
+
+            SqlDataAdapter da = new SqlDataAdapter("select * from stud",cn);
+
+            DataTable dt = new DataTable();
+
+            da.Fill(dt);
+
+            GridView1.DataSource = dt;
+
+            GridView1.DataBind();
+        }
+    }
+    protected void Insert_Click(object sender, EventArgs e)
+    {
+        string x = WebConfigurationManager.ConnectionStrings["cn"].ConnectionString;
+        SqlConnection cn = new SqlConnection(x);
+        SqlDataAdapter da = new SqlDataAdapter("select * from stud", cn);
+
+        DataTable dt = new DataTable();
+        da.Fill(dt);
+
+        DataRow dr = dt.NewRow();
+
+        dr["id"] = txtid.Text;
+        dr["name"] = txtname.Text;
+        dr["city"] = txtcity.Text;
+
+        dt.Rows.Add(dr);
+
+        SqlCommandBuilder cb = new SqlCommandBuilder(da);
+
+        da.Update(dt);
+
+        GridView1.DataSource = dt;
+
+        GridView1.DataBind();
+
+    }
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+        txtid.Text = "";
+        txtname.Text = "";
+        txtcity.Text = "";
+    }
+}
